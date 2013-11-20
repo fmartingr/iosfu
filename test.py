57e0a20da7ffc5ad68ded5ebca9866b143740d54
Felipe Martin <fmartingr@me.com>
2013-11-20 22:49:52 +0100

    Ignore manual scripts

--- test.py
+++
@@ -1,9 +0,0 @@
-from iosfu.plugin.library import Library
-from iosfu.backup import BackupManager
-
-manager = BackupManager()
-manager.lookup()
-
-lib = Library()
-lib.discover()
-print(lib.plugins)
