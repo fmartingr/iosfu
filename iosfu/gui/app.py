48591e76b1b7a5db7db99a88faf7006c25c25441
Felipe Martín <fmartin@bytepix.com>
2013-11-25 10:49:45 +0100

    GUI app core WIP

--- iosfu/gui/app.py
+++ iosfu/gui/app.py
@@ -1,13 +1,36 @@
 from flask import Flask
+
 from iosfu.conf import ROOT_PATH
+from iosfu.plugin.library import Library
+from iosfu.gui.core import GUIController
+
 
 server = Flask(__name__)
+
+library = Library()
+library.discover()
+
+controller = GUIController()
+controller.load_from_library(library)
+
+
+print(controller._panels)
 
 
 @server.route("/")
 def main():
     result = """
         Executed from: {0}<br />
-        Path for GUI module: {1}
     """.format(ROOT_PATH)
     return result
+
+
+@server.route("/<panel>/")
+def panel(panel_id=None):
+    panel = controller.load_panel(panel_id)
+    return panel
+
+
+@server.route("/<panel>/<section>/")
+def section(panel_id=None, section_id=None):
+    return 'lol'
