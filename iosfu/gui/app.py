4c264692fb783319e75b5251477ac382f9c0e094
Felipe Martín <fmartin@bytepix.com>
2013-11-26 16:55:15 +0100

    Base GUI layout with main layout template and letting using choose a backup if it's valid

--- iosfu/gui/app.py
+++ iosfu/gui/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, session
+from flask import Flask, session, render_template, redirect, url_for
 
 from iosfu.plugin.library import Library
 from iosfu.gui.core import GUIController
+from iosfu.backup import BackupManager
 
 
 server = Flask(__name__)
@@ -17,6 +18,15 @@ library.discover()
 controller = GUIController()
 controller.load_from_library(library)
 
+# Backups
+backup_manager = BackupManager()
+backup_manager.lookup()
+
+
+@server.context_processor
+def backup_list():
+    return dict(backups=backup_manager.backups)
+
 
 #
 # ROUTES
@@ -26,7 +36,7 @@ def main():
     """
     Main page
     """
-    return 'Main page'
+    return render_template('main.jinja')
 
 
 @server.route("/<panel_id>")
@@ -46,3 +56,23 @@ def section(panel_id=None, section_id=None):
     panel = controller.load_panel(panel_id)
     section = panel.get_section(section_id)
     return "{}.{}".format(panel.__slug__, section.__slug__)
+
+
+@server.route("/backup/<backup_id>/")
+def select_backup(backup_id):
+    """
+    Backup selection route
+    """
+    try:
+        backup = backup_manager.backups[backup_id]
+        if backup.valid:
+            session['backup'] = backup_id
+        else:
+            del session['backup']
+    except:
+        # Backup is not loaded / do not exist
+        # session['backup'] does not exist
+        if 'backup' in session:
+            del session['backup']
+
+    return redirect(url_for('main'))
