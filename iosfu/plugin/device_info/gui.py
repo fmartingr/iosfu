1ff41fb07010081e3fbbb8e268b48e10d228aa9b
Felipe Martin <fmartingr@me.com>
2014-03-23 19:53:42 +0100

    Device Info plugin now show device image (only for iphone)

--- iosfu/plugin/device_info/gui.py
+++ iosfu/plugin/device_info/gui.py
@@ -11,6 +11,33 @@ class Main(Section):
     plugin = DeviceInfoPlugin
     template = 'main.jinja'
 
+    def get_device_image(self, product):
+        device_map = (
+            ('iPhone1,1', 'iphone.png', ),
+            ('iPhone1,2', 'iphone3g.png', ),
+            ('iPhone2,1', 'iphone3gs.png', ),
+            ('iPhone3,', 'iphone4.png', ),
+            ('iPhone4,1', 'iphone4s.png', ),
+            ('iPhone5,1', 'iphone5.png', ),
+            ('iPhone5,2', 'iphone5.png', ),
+            ('iPhone1,3', 'iphone5c.png', ),
+            ('iPhone1,4', 'iphone5c.png', ),
+            ('iPhone6,', 'iphone5s.png', ),
+
+        )
+
+        return device_map[
+            [i for i, v in enumerate(device_map) if v[0] in product][0]
+        ][1]
+
+    def extra_context(self, ctx):
+        extra = {
+            'device_image': self.get_device_image(
+                ctx['device']['product_type']),
+        }
+
+        return extra
+
 
 class VersionInfo(Section):
     name = 'Version Info'
